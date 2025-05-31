1d9fbc34ba3bc1f3019fedf2d61b17f8670cf096
BLzCodes <mdtaiaburrajin@gmail.com>
2025-06-01 01:24:38 +0600

    Update file_handling_wizard.py
    +added file renaming module
    +added small features
    +optimized for better terminal readability & performance
    +cleaned up some useless code

--- file_handling_wizard.py
+++ file_handling_wizard.py
@@ -9,6 +9,7 @@ import shutil
 
 
 # color & format codes
+#----------------------------------------------------------------------------------------------------------------
 rst = "\033[0m"
 red = "\033[0;31m"
 grn = "\033[1;32m"
@@ -17,11 +18,13 @@ prp = "\u001b[35;1m"
 cyn = '\033[36m'
 
 bnk = "\033[5m"
+#----------------------------------------------------------------------------------------------------------------
 
 
 
 
 # extra shit
+#----------------------------------------------------------------------------------------------------------------
 root = tk.Tk()
 root.withdraw()
 
@@ -29,12 +32,14 @@ def clear_console():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 folder_to_create = ["Archives", "Images", "Videos", "Audios", "Texts", "Documents", "Executables", "Others"]
+#----------------------------------------------------------------------------------------------------------------
 
 
 
 
 
 # main function
+#----------------------------------------------------------------------------------------------------------------
 def main():
 
     clear_console()
@@ -50,12 +55,14 @@ def main():
 {grn}---------------------------------------------------------------------------------------------{rst}
 
 {prp}1. S O R T   F I L E S   B Y   T Y P E
+2. B U L K   N A M E   C H A N G E R
+
 0. E X I T{rst}
 
           """)
     
     try:
-        user_input = int(input(f"{grn}[ Select an option ] > {rst}"))
+        user_input = int(input(f"{grn}[ Select an option ] >>> {rst}"))
     except:
         input(f"\n\n{red}[ *breaks monitor ]\n\n[ Enter a valid number ... ]{rst}")
         clear_console()
@@ -63,30 +70,36 @@ def main():
 
     match user_input:
         case 1:
-            sort_by_type()
+            sorting_opt_one()
+        case 2:
+            renaming_opt_two()
         case 0:
             sys.exit()
         case _:
             input(f"\n\n{red}[ Enter a number that represents an option ... ]{rst}")
             clear_console()
             main()
+#----------------------------------------------------------------------------------------------------------------
 
 
 
 
 
 # 1. Sorter
-def sort_by_type():
-
+#----------------------------------------------------------------------------------------------------------------
+def sorting_opt_one():
+    global counter
+    counter = 0
     clear_console()
+
     print(f"""{prp}-----------------------------------------------------------------------\n
 [ This tool sorts files into separate folders depending on their type ]
 
 -----------------------------------------------------------------------{rst}
 
 """)
+    
     input(f"{ylw}{cyn}[ Press {ylw}ENTER{cyn} to select a folder & sort files inside ... ]{rst}")
-
     folder = filedialog.askdirectory()
     print(f"{grn}[ Selected folder: {folder} ]{rst}")
 
@@ -101,14 +114,17 @@ def sort_by_type():
     folder_creation()
 
     print(f"{ylw}{cyn}[ Sorting... ]{rst}\n")
-    for files in target_path.iterdir():
-        if files.is_file():
-            sorting(files, target_path)
-            print(f"[ Sorting: {files} ]")       
+
+    for file in target_path.iterdir():
+        if file.is_file():
+            sorting_core(file, target_path)
+            print(f"[ Sorted: {file} ]")
+            counter += 1       
         else:
             continue
-    
-    input(f"{grn}\n--------------------------------\n[ Task completed successfully! ]\n--------------------------------{rst}\n\n{ylw}{cyn}[ Press {ylw}ENTER{cyn} to continue ... ]{rst}")
+
+    print(f"\n\n{grn}[ Sorted {counter} files ]{rst}")
+    input(f"{grn}\n--------------------------------\n[ Task completed successfully! ]\n--------------------------------{rst}\n\n{ylw}{cyn}[ Press {ylw}ENTER{cyn} to return ... ]{rst}")
 
     main()
 
@@ -117,13 +133,18 @@ def folder_creation():
     print(f"\n\n{ylw}{cyn}[ Required sorting folders will be created in the selected directory ]{rst}")
 
     for folder_ in folder_to_create:
-
-        (target_path / folder_).mkdir(parents=True, exist_ok=True)
+        path = target_path / folder_
+        if not (path.exists() and path.is_dir()):
+            (path).mkdir(parents=True, exist_ok=True)
+            print(f"[ Created folder {folder_} ]")
+        else:
+            continue
  
-    input(f"{grn}[ Missing folders have been created ]\n\n\n{rst}{ylw}{cyn}[ Press {ylw}ENTER{cyn} to start sorting ... ]{rst}")
+    input(f"{grn}[ Missing folders have been created ]\n\n\n{rst}{ylw}{cyn}[ Press {ylw}ENTER{cyn} to start the sorting process ... ]{rst}")
+    
     clear_console()
 
-def sorting(file_path, target_path):
+def sorting_core(file_path, target_path):
 
     match file_path.suffix:
         case ".zip" | ".rar" | ".7z" | ".tar" | ".gz":
@@ -136,13 +157,69 @@ def sorting(file_path, target_path):
             shutil.move(file_path, target_path / "Audios")
         case ".txt" | ".docx" | ".doc" | ".odt":
             shutil.move(file_path, target_path / "Texts")
-        case ".pdf" | ".csv" :
+        case ".pdf" | ".csv":
             shutil.move(file_path, target_path / "Documents")
         case ".exe":
             shutil.move(file_path, target_path / "Executables")
         case _:
             shutil.move(file_path, target_path / "Others")
+#----------------------------------------------------------------------------------------------------------------
 
+
+
+
+
+# 2. Bulk name changer
+#----------------------------------------------------------------------------------------------------------------
+def renaming_opt_two():
+    index = 1
+    counter = 0
+    clear_console()
+
+    print(f"""{prp}---------------------------------------------------------\n
+[ This tool can be used to rename all files in a folder ]
+
+---------------------------------------------------------{rst}
+
+""")
+    input(f"{ylw}{cyn}[ Press {ylw}ENTER{cyn} to select a folder & rename files inside ... ]{rst}")
+    folder = filedialog.askdirectory()
+    print(f"{grn}[ Selected folder: {folder} ]{rst}")
+    target_path = Path(folder)
+
+    if not folder:
+        input(f"{red}[ No folder was selected ]{rst}\n\n\n{ylw}{cyn}[ Press {ylw}ENTER{cyn} to return ... ]{rst}")
+        clear_console()
+        main()
+
+    new_name = input(f"{ylw}{cyn}\n\n[ What should be the files renamed to? ] >>> {rst}")
+    print(f"{grn}[ Files will be renamed to: {new_name}_(index) ]{rst}")
+    input(f"\n\n{rst}{ylw}{cyn}[ Press {ylw}ENTER{cyn} to start the renaming process ... ]{rst}")
+    
+    clear_console()
+
+    print(f"{ylw}{cyn}[ Renaming ... ]{rst}\n")
+
+    for file in target_path.iterdir():
+        try:
+            if file.is_file():
+                old_path = Path(file)
+                new_path = old_path.with_name(new_name + "_" + str(index) + old_path.suffix)
+                print(f"[ Renamed: {file} ]")
+                index += 1
+                old_path.rename(new_path)
+                counter += 1
+            else:
+                continue
+        except:
+            input(f"{red}[ Invalid characters found in provided name. Please try again ]{rst}\n\n{ylw}{cyn}[ Press {ylw}ENTER{ylw}{cyn} to return ... ]{rst}")
+            main()
+
+    print(f"\n\n{grn}[ Renamed {counter} files ]{rst}")
+    input(f"{grn}\n--------------------------------\n[ Task completed successfully! ]\n--------------------------------{rst}\n\n{ylw}{cyn}[ Press {ylw}ENTER{cyn} to return ... ]{rst}")
+
+    main()
+#----------------------------------------------------------------------------------------------------------------
 
 
 
